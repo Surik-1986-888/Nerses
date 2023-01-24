1c78f4f63f51dde8827890f5e216d4d560750ac0
Surik <sur_mkrtchyan@mail.ru>
2023-01-24 20:49:33 +0400

    my first commit

---
+++ bbb.py
@@ -0,0 +1,11 @@
+# with open('aaaa.txt','r') as f:
+# 	datan=f.read()
+
+
+# with open('aaaa.txt,'w') as f:
+# 	aaaa='ad new somesing'
+# 	f.write(aaaa)
+
+with open('aaaa.txt', 'w') as f:
+    aaaa = 'some data to be written to the file'
+    f.write(aaaa)
